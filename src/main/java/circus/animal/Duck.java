2a44eb920afad99e25ae6cf6841d9e07304a2680
nicholascxh <02nicholaschong@gmail.com>
2025-02-16 15:10:07 +0800

    no message

---
+++ src/main/java/circus/animal/Duck.java
@@ -0,0 +1,17 @@
+package circus.animal;
+
+public class Duck extends Bird {
+    @Override
+    public String speak() {
+        return "Quack Quack";
+    }
+
+    @Override
+    public String toString() {
+        return "I'm a Duck";
+    }
+
+    public void swim() {
+        System.out.println("I'm swimming...");
+    }
+}
